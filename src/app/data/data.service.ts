5353c73648c4ea7a87d02dc6a31006e25a91b498
Alex Lamb <alexlamb104@gmail.com>
2022-08-30 16:50:49 -0600

    Making an http post request successfuly

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserSettings } from './user-settings';
@@ -7,8 +8,9 @@ import { UserSettings } from './user-settings';
 })
 export class DataService {
 
-  constructor() { }
-  postUserSettingsForm(userSettings: UserSettings) : Observable<UserSettings>{
-    return of(userSettings);
+  constructor(private http: HttpClient) { }
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any>{
+    return this.http.post('https://putsreq.com/lwxRdQoh0lNKcdlOYoaz', userSettings);
+    // return of(userSettings);
   }
 }
